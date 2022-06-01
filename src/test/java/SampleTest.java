c991216f5d66d964f22f56c7604431d456a5bf32
saikrishna321 <saikrishna321@yahoo.com>
2022-06-01 16:06:30 +0530

    initial commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,44 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+
+public class SampleTest {
+    public AppiumDriver driver;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 700000);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UIAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/VodQA.apk");
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @Test
+    public void SampleTest() {
+        wait = new WebDriverWait(driver, 30);
+        wait.until(presenceOfElementLocated(MobileBy.AccessibilityId("login"))).click();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+}
